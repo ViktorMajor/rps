2612f7625ae1b940273e6b81e3ff9e0f2c22eda0
Major Viktor <majorviktor95@gmail.com>
2023-02-10 12:42:18 +0100

    finsih

--- rps.js
+++ rps.js
@@ -1,7 +1,89 @@
 let userPoints = 0;
 let computerPoints = 0;
 
-while(userPoints < 2 && computerPoints < 2) {
+function logUserChoice(id) {
+  let userChoice = id;
+
+  let computerAnswer = getComputerChoice();
+
+  let result = document.querySelector(".er");
+  let vs = document.querySelector(".vs");
+
+switch (userChoice) {
+    case "rock":
+      switch (computerAnswer) {
+        case "rock":
+          vs.innerHTML = "Kő vs Kő"
+          result.innerHTML = "Döntetlen!";
+          break;
+        case "paper":
+          vs.innerHTML = "Kő vs Papír"
+          result.innerHTML = "Vesztettél!";
+          computerPoints++;
+          break;
+        case "scissors":
+          vs.innerHTML = "Kő vs Olló"
+          result.innerHTML = "Győztél";
+          userPoints++;
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerAnswer) {
+        case "rock":
+          vs.innerHTML = "Papír vs Kő"
+          result.innerHTML = "Győztél";
+          userPoints++;
+          break;
+        case "paper":
+          vs.innerHTML = "Papír vs Papír"
+          result.innerHTML = "Döntetlen!";
+          break;
+        case "scissors":
+          vs.innerHTML = "Papír vs Olló"
+          result.innerHTML = "Vesztettél!";
+          computerPoints++;
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerAnswer) {
+        case "rock":
+          vs.innerHTML = "Olló vs Kő"
+          result.innerHTML = "Vesztettél!";
+          computerPoints++;
+          break;
+        case "paper":
+          vs.innerHTML = "Olló vs Papír"
+          result.innerHTML = "Győztél";
+          userPoints++;
+          break;
+        case "scissors":
+          vs.innerHTML = "Olló vs Olló"
+          result.innerHTML = "Döntetlen!";
+          break;
+      }
+      break;
+  }
+
+
+  let your_p = document.querySelector(".your_p");
+your_p.innerHTML = "A te pontjaid: " + userPoints;
+  let com_p = document.querySelector(".com_p");
+com_p.innerHTML = "A gép pontjai: " + computerPoints;
+
+}
+
+document.querySelector("#rock").addEventListener("click", function() {
+  logUserChoice("rock");
+});
+document.querySelector("#paper").addEventListener("click", function() {
+  logUserChoice("paper");
+});
+document.querySelector("#scissors").addEventListener("click", function() {
+  logUserChoice("scissors");
+});
+
 let getComputerChoice = () => {
   let randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
@@ -9,55 +91,6 @@ let getComputerChoice = () => {
       return "rock";
     case 1:
       return "paper";
-      case 2:
-        return "scissors";
-  }
-};
-let userAnswer1 = (prompt("Rock, paper vagy scissors?"));
-let userAnswer = userAnswer1.toLowerCase();
-let computerAnswer = getComputerChoice();
-
-console.log("The computer answer is: "+ computerAnswer);
-console.log("Your answer is:"+userAnswer);
-if (userAnswer === "rock" && computerAnswer === "scissors") {
-  console.log("so You win!");
-  userPoints++;
-
-} else if (userAnswer === "rock" && computerAnswer === "paper") {
-  console.log("so You lose!");
-  computerPoints++;
-  return
-
-} else if (userAnswer === "rock" && computerAnswer === "rock") {
-  console.log("so It was draw!");
-  return
-
-} else if (userAnswer === "paper" && computerAnswer === "scissors") {
-  console.log("so You lose!");
-  computerPoints++;
-
-} else if (userAnswer === "paper" && computerAnswer === "paper") {
-  console.log("so It was draw!");
-
-} else if (userAnswer === "paper" && computerAnswer === "rock") {
-  console.log("so You win!");
-  userPoints++;
-
-} else if (userAnswer === "scissors" && computerAnswer === "paper") {
-  console.log("so You win!");
-  userPoints++;
-  
-} else if (userAnswer === "scissors" && computerAnswer === "rock") {
-  console.log("so You lose!");
-  computerPoints++;
-
-} else if (userAnswer === "scissors" && computerAnswer === "scissors") {
-  console.log("so It was draw!");
-  
-} else {
-  console.log("so You lose!");
-}
-      
-            
-console.log("Your ponts: "+userPoints);
-console.log("The computers points: "+computerPoints);}
+    case 2:
+      return "scissors";
+}}
